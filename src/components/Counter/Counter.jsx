931c3b2f78840134b0a7913bcb26c6b08d008650
Anton Ziuzikov <zuzikov.anton@gmail.com>
2024-02-14 10:46:33 +0300

    feat: add keys to components for proper updating

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,17 +1,15 @@
-import { useState, useCallback, useMemo } from 'react';
+import {useState, memo, useCallback, useMemo, useEffect} from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
+import CounterHistory from "./CounterHistory";
 
 function isPrime(number) {
-  log(
-    'Calculating if is prime number',
-    2,
-    'other'
-  );
+  log('Calculating if is prime number', 2, 'other');
+
   if (number <= 1) {
     return false;
   }
@@ -27,40 +25,56 @@ function isPrime(number) {
   return true;
 }
 
-const Counter = function Counter({ initialCount }) {
+const Counter = memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
-  const [counter, setCounter] = useState(initialCount);
+  const initialCountIsPrime = useMemo(
+      () => isPrime(initialCount),
+      [initialCount]
+  );
+
+  // useEffect(() => {
+  //   setCounterChanges([{value: initialCount, id: Math.random() * 1000}]);
+  // }, [initialCount]);
+
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([{ value: initialCount, id: Math.random() * 1000}]);
+
+  const currentCounter = counterChanges.reduce(
+      (prevCounter, counterChange) => prevCounter + counterChange.value,
+      0
+  );
 
   const handleDecrement = useCallback(function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [{ value: -1, id: Math.random() * 1000},
+                                                          ...prevCounterChanges]);
   }, []);
 
   const handleIncrement = useCallback(function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [{ value: 1, id: Math.random() * 1000},
+                                                          ...prevCounterChanges]);
   }, []);
 
-  //useCallback used here because handle functions are recreated each time counter changes
-  //so "memo" from IconButton always receive different links for handlers and keeps rerendering
-
   return (
-    <section className="counter">
-      <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{' '}
-        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
-      </p>
-      <p>
-        <IconButton icon={MinusIcon} onClick={handleDecrement}>
-          Decrement
-        </IconButton>
-        <CounterOutput value={counter} />
-        <IconButton icon={PlusIcon} onClick={handleIncrement}>
-          Increment
-        </IconButton>
-      </p>
-    </section>
+      <section className="counter">
+        <p className="counter-info">
+          The initial counter value was <strong>{initialCount}</strong>. It{' '}
+          <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+        </p>
+        <p>
+          <IconButton icon={MinusIcon} onClick={handleDecrement}>
+            Decrement
+          </IconButton>
+          <CounterOutput value={currentCounter} />
+          <IconButton icon={PlusIcon} onClick={handleIncrement}>
+            Increment
+          </IconButton>
+        </p>
+        <CounterHistory history={counterChanges}/>
+      </section>
   );
-}
+});
 
 export default Counter;
--- src/components/Counter/CounterHistory.jsx
+++ src/components/Counter/CounterHistory.jsx
@@ -21,10 +21,12 @@ function HistoryItem({ count }) {
 export default function CounterHistory({ history }) {
   log('<CounterHistory /> rendered', 2);
 
+  /*it's preferable to use keys related to specific values, not the index*/
+  /*because index of corresponding element may change during addition of new elements*/
   return (
     <ol>
       {history.map((count, index) => (
-        <HistoryItem key={index} count={count} />
+        <HistoryItem key={count.id} count={count.value} />
       ))}
     </ol>
   );
